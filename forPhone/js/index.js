7f4f3163f6bdfe45cf516f14a85eb76461bf1f9d
zhaojianbo456852 <461501699@qq.com>
2017-05-02 11:42:40 +0800

    part1 finish

--- forPhone/js/index.js
+++ forPhone/js/index.js
@@ -1,3 +1,13 @@
 $('div.mag').swipeleft(function(){
 	$('div.mag').hide(2000);
+	$('div.mag').show(2000);
+	$(function(){
+		function jump(){
+
+			$('.part1').css('background-image','url(images/image2.jpg)');
+			// location.href = 'http://www.baidu.com';
+		}
+	setTimeout(jump,4000);
+	});
 });
+
